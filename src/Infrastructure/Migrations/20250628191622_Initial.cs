e86ffbc2b32ce3244c047ffb561468c1472de8b1
Catofood <krylov.iluha@gmail.com>
2025-06-28 22:40:34 +0300

    Миграция

---
+++ src/Infrastructure/Migrations/20250628191622_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SenderCity = table.Column<string>(type: "text", nullable: false),
+                    SenderAddress = table.Column<string>(type: "text", nullable: false),
+                    ReceiverCity = table.Column<string>(type: "text", nullable: false),
+                    ReceiverAddress = table.Column<string>(type: "text", nullable: false),
+                    PackageWeightKg = table.Column<decimal>(type: "numeric", nullable: false),
+                    PackagePickupDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
